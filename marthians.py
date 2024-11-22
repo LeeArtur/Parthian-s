1fbadf9e974bd6ab8914d7fb0b79b539e7d43e47
Lee Arthur <leearthur@MacBook-Air-Lee.local>
2024-11-22 11:11:06 +0600

    fixed loop mistake

--- marthians.py
+++ marthians.py
@@ -1,23 +1,36 @@
 import random
 
-ship_coordinates = [2,6,5]
-weight = [200, 300, 213]
+ship_coordinates = [2, 6, 5]
+weights = [200, 300, 213]
 total_weight = 713
-user_coordinates = []
-sum_weight = 0
-for i in range(3):
-    while True:
-        user_input = input(f"Enter coordinate number {i + 1}: ")
-        if user_input.isdigit():
-            user_coordinates.append(int(user_input))
-            break
-        else:
-            print("Invalid input. Try again!")
 
-for i in range(3):
-    if user_coordinates[i] in ship_coordinates:
-        sum_weight += weight[ship_coordinates.index(user_coordinates[i])]
-        print(sum_weight)
-        if sum_weight == total_weight:
-            print("Congrats")
-#gfjhwsjdhwgdjhgwgujgsghj
+def asking_user():
+    user_coordinates = []
+    for i in range(3):
+        while True:
+            user_input = input(f"Enter coordinate number {i + 1}: ")
+            if user_input.isdigit():
+                user_coordinates.append(int(user_input))
+                break
+            else:
+                print("Invalid input. Please enter a positive integer.")
+    return user_coordinates
+
+def randomizer():
+    return [random.randint(1, 10) for _ in range(3)]
+
+while True:
+    user_coordinates = asking_user()
+
+    sum_weight = 0
+    for coord in user_coordinates:
+        if coord in ship_coordinates:
+            sum_weight += weights[ship_coordinates.index(coord)]
+
+    if sum_weight == total_weight:
+        print("Congratulations! You've found all the cargo!")
+        break
+    else:
+        print(f"Total weight found: {sum_weight} kg. Try again!")
+        ship_coordinates = randomizer()
+        print(f"New ship coordinates: {ship_coordinates}")
